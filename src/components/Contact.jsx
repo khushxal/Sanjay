5203a5b4d5048e65584eb7d2373cae851fb7b1b6
Khushxal <vkkhushal18@gmail.com>
2024-08-16 15:19:02 +0530

    Changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ function Contact() {
       <h1>Contact Me</h1>
       <hr />
       <div className="row my-5">
-        <div className="col text-lg-center">
+        <div className="col mb-2 text-lg-center">
           <img
             className="p-1"
             src="https://img.icons8.com/?size=100&id=89362&format=png&color=000000"
@@ -18,7 +18,7 @@ function Contact() {
             <a href="mailto:rm_indore10@yahoo.com">rm_indore10@yahoo.com</a>{" "}
           </span>
         </div>
-        <div className="col text-lg-center">
+        <div className="col mb-2 text-lg-center">
           <img
             src="https://img.icons8.com/?size=100&id=8808&format=png&color=000000"
             className="p-1"
@@ -32,7 +32,7 @@ function Contact() {
             </a>{" "}
           </span>
         </div>
-        <div className="col text-lg-center">
+        <div className="mb-2 text-lg-center">
           <img
             className="p-1"
             src="https://img.icons8.com/?size=100&id=9729&format=png&color=000000"
@@ -41,11 +41,11 @@ function Contact() {
             width={40}
           />
           <span className="ms-2 fs-3">
-            <a href="tel:+919826189859">+91 9826189859</a>
+            <a href="tel:+919826189859">9826189859</a>
           </span>
         </div>
       </div>
-      <div className="row my-4 d-flex flex-row gap-5 justify-content-center text-center">
+      {/* <div className="row my-4 d-flex flex-row gap-5 justify-content-center text-center">
         <div className="col-lg-5 ">
           <img
             className="p-1"
@@ -56,7 +56,7 @@ function Contact() {
           />
           <span className="ms-2 fs-3">Indore, India</span>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
